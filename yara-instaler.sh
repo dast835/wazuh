5dd09069438b4c11e7a469da937fe9c4ca833e70
Daniel Sternik <33752875+dast835@users.noreply.github.com>
2023-06-02 12:03:01 +0200

    Update yara-instaler.sh

--- yara-instaler.sh
+++ yara-instaler.sh
@@ -10,6 +10,7 @@ cd /usr/local/bin/yara-4.3.1/
 make
 make install
 cd /usr/local
+git clone https://github.com/Neo23x0/signature-base.git
 wget https://github.com/dast835/wazuh/raw/main/yara_rules_updater.sh
 
 ##########################
